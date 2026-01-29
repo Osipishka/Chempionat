c8e0caa2983ab7111abc7345077d76fb88acc6d7
Андрюха Смер АС. Смерч <Andro@CCJGMK.local>
2026-01-29 13:55:23 +0300

    Add flask

--- BestGames - Html CSS_С/app.py
+++ BestGames - Html CSS_С/app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16) 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('pass')
+        print(login, password)
+    return render_template('login.html') 
+
 @app.route('/register')
 @app.route('/register.html')
 def register():
     return render_template('register.html')
-
-# Вход
-@app.route('/login')
-@app.route('/login.html')
-def login():
-    return render_template('login.html')
 
 # Профиль
 @app.route('/profile')
@@ -33,16 +39,6 @@ def catalog():
 def admin():
     return render_template('admin.html')
 
-# Обслуживаем статические файлы (CSS, JS, изображения)
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
-
-# Обработка 404 ошибки
-@app.errorhandler(404)
-def page_not_found(e):
-    # Можно вернуть свою страницу 404
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
